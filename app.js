760f6b8e772b36eb702a9e1dde55b636ff55e8b3
darrenyong <contact.darrenyong@gmail.com>
2020-03-26 11:50:43 -0700

    Connect to Mongo

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
+
+mongoose.connect(db, { useNewUrlParser: true }).then(() => {
+  console.log('Connected to MongoDB successfully')
+}).catch(err => {console.log(err)})
 
 app.listen(port, () => {});
 app.use(bodyParser.urlencoded({ extended: false }));
