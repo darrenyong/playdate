89683a131af9d1572f6a2e71926045a632fc4e6d
darrenyong <contact.darrenyong@gmail.com>
2020-03-27 14:40:16 -0700

    Add passport middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
 const app = express();
 const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
@@ -14,6 +16,9 @@ mongoose.connect(db, { useNewUrlParser: true }).then(() => {
 app.listen(port, () => {});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.get('/', (req, res) => {
   res.json( {msg: 'This is a test'})
