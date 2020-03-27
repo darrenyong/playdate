e0282b0c20d947f36e0c6e86d96139125ccfd1e3
darrenyong <contact.darrenyong@gmail.com>
2020-03-26 17:46:20 -0700

    Add /users/register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,4 +8,26 @@ router.get('/test', (req, res) => {
   res.json({ msg: "This is the users route" });
 });
 
+router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'There is already a user with that e-mail';
+      return res.status(400).json(errors);
+    }
+
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    })
+    return res.status(200)
+  })
+})
+
 module.exports = router;
