7d378b64448e8542673e2872de5f5c345c4d1e84
darrenyong <contact.darrenyong@gmail.com>
2020-03-27 11:04:27 -0700

    Salt and hash password before saving

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
+const User = require('../../models/User')
 
 router.get('/test', (req, res) => {
   res.json({ msg: "This is the users route" });
@@ -22,10 +24,23 @@ router.post('/register', (req, res) => {
     }
 
     const newUser = new User({
-      name: req.body.name,
+      handle: req.body.handle,
       email: req.body.email,
       password: req.body.password
     })
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser.save().then(user => {
+          res.status(200).json(user).catch(err => {
+            console.log(err);
+          })
+        })
+      })
+    })
+
     return res.status(200)
   })
 })
